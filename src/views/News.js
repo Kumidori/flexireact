d60f560a38b0999c9e72de6fe127e1e76ed87e28
laptop <jojok1342@gmail.com>
2018-02-05 16:21:30 +0100

    Newsblocks zu einzelnen, sortierten News

--- src/views/News.js
+++ src/views/News.js
@@ -12,6 +12,9 @@ import $ from "jquery";
 // RSS FEED PER JQUERY HOLEN UND EINBAUEN
 
 $( document ).ready(function() {
+
+    var eintraege = [];
+
     $.get('https://cors-anywhere.herokuapp.com/https://felix.hs-furtwangen.de/rss/personal/beckerth.hfu/XLZrRj/olat.rss', function (data) {
         $(data).find("item").each(function () {
             console.log(this);
@@ -19,15 +22,53 @@ $( document ).ready(function() {
             var title = item['title'];
             var link = item['link'];
             var description = item['description'];
+            if(description != "Mit diesem Link gelangen Sie zu FELIX."){
+                var short_description = description.split("<ul class='list-unstyled'><li>");
+                short_description = short_description[1].split("</ul>");
+                short_description = short_description[0].split("</li><li>");
+                short_description.forEach(function(element) {
+                    var date_time=element.split("<span class='o_nowrap o_date'>am ");
+                    var date_time=date_time[1].split("</span>");
+                    var date_time=date_time[0];
+                    var date= date_time.slice(0, 10);
+                    var time= date_time.slice(11, 17);
+                    
+                    // console.log(time);
+                    
+                    var message=element.split(">");
+                    var message=message[3].split("</a");
+                    // var message=message[0].split(" von ");
+                    var autor=message[1];
+                    var message=message[0];
+                    // console.log(message, autor);
+                    eintraege.push([+date[3]+date[4]+" "+date[0]+date[1]+", "+ date[6]+date[7]+date[8]+date[9]+" "
+                    
+                    
+                    +time[0]+time[1]+":"
+                    +time[3]+time[4]+":00"
+                    ,message,date,time,title]);
+                });
+            }
             
-
-            var new_card = '<div class="card light-green darken-3"><i class="material-icons left white-text">chat_bubble_outline</i><div class="card-content white-text"><span class="card-title">'+title+'</span>'+description+'</div><div class="card-action white-text"><a class="white-text"></a><a href="'+link+'" class="right white-text">Link</a></div></div>';
+        });
+        console.log(eintraege);
+        eintraege.sort(function(a, b) {
+            var dateA = new Date(a[0]), dateB = new Date(b[0]);
+            console.log(dateA);
+            return dateB - dateA;
+        });
+        console.log(eintraege);
+        console.log(new Date("2018-02-03-T19:23:00Z"));
+        eintraege.forEach(function(eintrag) {
+            console.log(eintrag);
+            var new_card = '<div class="card light-green darken-3"><i class="material-icons left white-text">chat_bubble_outline</i><div class="card-content white-text"><span class="card-title">'+eintrag[2]+' '+eintrag[3]+' '+eintrag[4]+'</span>'+eintrag[1]+'</div><div class="card-action white-text"><a class="white-text"></a><a href="'+eintrag[0]+'" class="right white-text">Link</a></div></div>';
             
             var news_content = $('.col.s12.m12.testzugriffperjquery').html();
             $('.col.s12.m12.testzugriffperjquery').html(news_content+new_card);
-            console.log(link);
-        });
+            // console.log(link);
+          });
     });
+
 });
 
 
